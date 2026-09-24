9aaf8ef32c6100095ab44d43142cae56c657b0f9
Jagadish <jagadishchandra998@gmail.com>
2026-09-24 22:51:34 +0530

    create authController

---
+++ Backend/src/controllers/authController.js
@@ -0,0 +1,120 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const registerUser = async (req, res) => {
+    try {
+
+        const { name, email, password, location } = req.body;
+
+        if (!name || !email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "Name, email and password are required"
+            });
+        };
+
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return res.status(400).json({
+                success: false,
+                message: "User already exists"
+            })
+        };
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+            location
+        });
+
+        res.status(200).json({
+            success: true,
+            message: "User registered successfully",
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+                location: user.location
+            }
+        });
+
+    }
+    catch (error) {
+        console.error("Register error:", error.message);
+
+        res.status(500).json({
+            message: "Server error"
+        });
+    }
+};
+
+export const loginUser = async (req, res) => {
+    try {
+
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "Email and password are required"
+            })
+        };
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(401).json({
+                message: "Invalid email or password"
+            })
+        };
+
+        const isPasswordCorrect = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        if (!isPasswordCorrect) {
+            return res.status(401).json({
+                message: "Invalid email or password"
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                userId: user._id,
+                role: user.role
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "7d"
+            }
+        );
+
+        res.status(200).json({
+            message: "Login successful",
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+                location: user.location
+            }
+        });
+
+    }
+    catch (error) {
+        console.log("Login Error", error);
+
+        res.status(500).json({
+            success: false,
+            message: "Server Error"
+        })
+    }
+};
---
+++ Backend/src/routes/authRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { registerUser,loginUser } from "../controllers/authController.js";
+
+const routes = express.Router();
+
+routes.post("/register",registerUser);
+routes.post("/login",loginUser);
+
+export default routes;
